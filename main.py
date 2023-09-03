fce7897bc9d6a4f2f1e4d78d4081763e283e0420
sciubilecki <sciubilecki.robert@gmail.com>
2023-09-03 19:40:59 +0200

    some edit

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 from PIL import Image
 
 
+# *************************** modify this as needed *********************************
 
 folder_path = 'C:/Users/rober/Desktop/VScode/html and css course/html-css-course-master/html-css-course-master/starter/My-templates/paintings'
 file_format = '.jpg'
 main_folder = 'paintings'
 low_res_folder = 'low-res'
 
+# ***********************************************************************************
 
 files_path = folder_path
 files = os.listdir(files_path)
@@ -17,6 +19,9 @@ for (index, file) in enumerate(files):
         img = Image.open(folder_path + '/' + file)
     except PermissionError:
         continue
+
+# **********************Modify dictionary keys names as needed***********************
+
     images.append({'title': file.replace('-', ' ').replace(file_format, ''),
                             'fullRes': f'{main_folder}/{file}',
                             'lowRes': f'{main_folder}/{low_res_folder}/{file}',
